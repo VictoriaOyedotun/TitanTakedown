02e347ed0c0bc0d185c9a1c90aabcc0f21bdc3e2
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2023-11-13 12:40:49 -0800

    feat: monster animations

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializedField]
+    private GameObject[] monsterReference;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

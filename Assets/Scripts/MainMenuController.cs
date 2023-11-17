25c59f851cfb23e9eccc280e6b54587db0efa7c2
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2023-11-17 09:24:42 -0800

    feat: selecting player to play game initial set up

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 public class MainMenuController : MonoBehaviour
 {
    public void PlayGame(){
-    Debug.Log("Button is pressed");
-    SceneManager.LoadScene("GamePlay");
+   //  string clickedObj = EventSystem.current.currentSelectedGameObject.name;
+   //  Debug.Log("Index: " + clickedObj);
+      // Debug.Log("Button is pressed");
+   //  SceneManager.LoadScene("GamePlay");
+   int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+   int[] a = new int[10];
+   a[selectedCharacter] = 10;
    }
 }
+
+

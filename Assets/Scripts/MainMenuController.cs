544075192845d37c1878b5365f687c917b741136
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2023-11-17 09:01:17 -0800

    feat: navigate to a scene

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
    public void PlayGame(){
-    
+    Debug.Log("Button is pressed");
+    SceneManager.LoadScene("GamePlay");
    }
 }

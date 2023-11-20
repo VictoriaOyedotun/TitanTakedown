8703b1e70fbf89d0a2d22623aafdc6251863eaf2
O-Victoria <99912502+O-Victoria@users.noreply.github.com>
2023-11-19 19:24:39 -0800

    feat: almost there...home butn and restart btn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,7 +32,9 @@ public class GameManager : MonoBehaviour
         SceneManager.sceneLoaded -= OnLevelFinishedLoading;
     }
     void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode){
-
+        if(scene.name == "Gameplay"){
+            Instantiate(characters[CharIndex]);
+        }
     }
 
 }

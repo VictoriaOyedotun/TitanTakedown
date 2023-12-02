0753c521594d67d0983e7d9f3d930b05a8dbe2d2
Kenneth Kouadio <34052222+kennethKing6@users.noreply.github.com>
2023-12-02 09:18:53 -0800

    feat:level initial script

---
+++ Assets/Scripts/LevelScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelScript : MonoBehaviour
+{
+    public static int LevelValue = 0;
+    Text level;
+    // Start is called before the first frame update
+    void Start()
+    {
+     level = GetComponent<Text>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        level.text = "level: " + LevelValue;
+    }
+}
